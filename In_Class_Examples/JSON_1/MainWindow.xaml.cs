7f5df65259875b2561956261a8f7105f42105730
Ackerman, Adam P <adamackrmn@gmail.com>
2022-09-12 11:45:29 -0500

    did json in class example

---
+++ In_Class_Examples/JSON_1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace JSON_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //process data here
+            string json = File.ReadAllText("MOCK_DATA.json");
+
+            List<Person> peeps = new List<Person>();
+
+            peeps = JsonConvert.DeserializeObject<List<Person>>(json);
+
+            foreach (Person person in peeps)
+            {
+                lstData.Items.Add(person); 
+            }
+
+        }
+    }
+}
