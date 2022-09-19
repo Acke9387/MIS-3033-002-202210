96e5d4b46d6584726d40af9cda02c20e0a6a2320
Ackerman, Adam P <adam@ou.edu>
2022-09-19 10:58:41 -0500

    Completed participation

--- Participations/JSON-FromAFile/MainWindow.xaml.cs
+++ Participations/JSON-FromAFile/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,50 @@ namespace JSON_FromAFile
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Car> allData;
         public MainWindow()
         {
             InitializeComponent();
+
+            string fileContents = File.ReadAllText("Mock_Data_Car_Owners.json");
+
+            allData = JsonConvert.DeserializeObject<List<Car>>(fileContents);
+            cboColors.Items.Add("All Colors");
+            cboColors.SelectedIndex = 0;
+
+            List<string> colors = new List<string>();
+            foreach (Car car in allData)
+            {
+                if ( !colors.Contains(car.Color))//!cboColors.Items.Contains(car.Color) )
+                {
+                    //cboColors.Items.Add(car.Color);
+                    colors.Add(car.Color);
+                }
+
+                lstCars.Items.Add(car);
+            }
+
+            foreach (string color in colors.OrderBy( x => x))
+            {
+                cboColors.Items.Add(color);
+            }
+
+            lblResults.Content = $"{lstCars.Items.Count.ToString("N0")} results found.";
+        }
+
+        private void cboColors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedColor = (string)cboColors.SelectedItem;
+            lstCars.Items.Clear();
+            foreach (Car c in allData)
+            {
+                if (c.Color == selectedColor)
+                {
+                    lstCars.Items.Add(c);
+                }
+            }
+
+            lblResults.Content = $"{lstCars.Items.Count.ToString("N0")} results found.";
         }
     }
 }
