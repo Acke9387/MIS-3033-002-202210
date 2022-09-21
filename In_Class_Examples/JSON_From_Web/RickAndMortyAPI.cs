5947284c5cc14ae8ec0fba692cb521fd4c0d89bd
Ackerman, Adam P <adam@ou.edu>
2022-09-21 11:41:22 -0500

    Completed an in class example over using an API to get JSON

---
+++ In_Class_Examples/JSON_From_Web/RickAndMortyAPI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_From_Web
+{
+    public class RickAndMortyAPI
+    {
+
+        public Info info { get; set; }
+
+        public List<Character> results { get; set; }
+
+    }
+
+    public class Character
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string image { get; set; }
+
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public string prev { get; set; }
+
+        public Info()
+        {
+            count = 0;
+            pages = 0;
+            next = string.Empty;
+            prev = string.Empty;
+        }
+
+    }
+}
