7da1a46f5f751f615553dfb821aacae67ca52cfa
Ackerman, Adam P <adam@ou.edu>
2022-11-07 11:19:23 -0600

    Created our first ASP.NET MVC Application

---
+++ In_Class_Examples/MyFirstMVCApplication/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstMVCApplication.Models;
+
+namespace MyFirstMVCApplication.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TeachingAssistants()
+        {
+            List<TA> tas = new List<TA>()
+            {
+                new TA(){ Name = "Matt" },
+                new TA(){ Name = "Connor" },
+                new TA(){ Name = "Jenna" },
+                new TA(){ Name = "Nicole" }
+            };
+            return View(tas);
+        }
+
+    }
+}
---
+++ In_Class_Examples/MyFirstMVCApplication/Models/TA.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace MyFirstMVCApplication.Models
+{
+    public class TA
+    {
+        [DisplayName("TA Name")]
+        public string Name { get; set; }
+
+    }
+}
